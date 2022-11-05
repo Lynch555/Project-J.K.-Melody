26c4e683a5fec74fa45e45f7753df18e4b0bafdf
Сергей <79439026+Lynch555@users.noreply.github.com>
2022-11-05 11:31:50 +0300

    Add files via upload
    Добавил интерактив на сайт с помощью языка JavaScript и библиотеки jQuery. Написал скрипт подсветки этажей и изменение счетчика этажей.

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+  var currentFloor = 2; // переменнная, где хранится текущий этаж 
+  var floorPath = $(".home-image path"); //каддый отдельный этаж в SVG
+  var counterUp = $(".counter-up"); // увеличение этажа
+  var counterDown = $(".counter-down"); // уменьшение этажа
+
+  // футкция при наведении мышкой на этаж
+  floorPath.on("mouseover", function () {
+    floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
+    currentFloor = $(this).attr("data-floor"); // получаем значение текущего этажа
+    $(".counter").text(currentFloor); // записываем значение этажа в счетчик
+  });
+
+  // отслеживаем клик по кнопке в верх
+  counterUp.on("click", function () {
+    // проверяем значение этажа, оно не должно быть больше 18
+    if (currentFloor < 18) {
+      currentFloor++; // прибавляем этаж
+      usCurrentFloor = currentFloor.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGrouping: false // форматируем переменную добавляя 0
+      });
+      $(".counter").text(usCurrentFloor); // записываем значение этажа в счетчик
+      floorPath.removeClass("current-floor"); // удаляем активный класс у этажей
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); //подсвечиваем текущий этаж
+    }
+  });
+  counterDown.on("click", function () {
+    if (currentFloor > 2) {
+      currentFloor--;
+      usCurrentFloor = currentFloor.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGrouping: false
+      });
+      $(".counter").text(usCurrentFloor);
+      floorPath.removeClass("current-floor");
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
+    }
+  });
+});
